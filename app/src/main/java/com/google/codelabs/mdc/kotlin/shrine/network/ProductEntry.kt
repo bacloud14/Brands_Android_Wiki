2d743ac2fe88b66fd4932ee1f7912ee3297cbeb0
bacloud14 <bacloud14@gmail.com>
2020-10-30 10:55:49 +0100

    Initial commit

---
+++ app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/network/ProductEntry.kt
@@ -0,0 +1,47 @@
+package com.google.codelabs.mdc.kotlin.shrine.network
+
+import android.content.res.Resources
+import android.net.Uri
+import com.google.codelabs.mdc.kotlin.shrine.R
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.io.BufferedReader
+import java.util.*
+
+/**
+ * A product entry in the list of products.
+ */
+class ProductEntry(
+        val title: String, dynamicUrl: String, val url: String, val price: String, val category: String, val description: String) {
+    val dynamicUrl: Uri = Uri.parse(dynamicUrl)
+
+    companion object {
+        private var target: Int = 0
+
+        /**
+         * Loads a raw JSON at R.raw.products and converts it into a list of ProductEntry objects
+         */
+
+        fun initProductEntryList(resources: Resources, s: String, limit: Int, random: Boolean): List<ProductEntry> {
+            target = when (s) {
+                "featured" -> R.raw.featured
+                "food" -> R.raw.featured
+                else -> { // Note the block
+                    R.raw.products
+                }
+            }
+            val inputStream = resources.openRawResource(target)
+            val jsonProductsString = inputStream.bufferedReader().use(BufferedReader::readText)
+            val gson = Gson()
+            val productListType = object : TypeToken<ArrayList<ProductEntry>>() {}.type
+            var list = gson.fromJson<List<ProductEntry>>(jsonProductsString, productListType)
+            when (random) {
+                true -> Collections.shuffle(list)
+            }
+            if (limit != 0) {
+                list = list.subList(0, list.size.coerceAtMost(limit))
+            }
+            return list
+        }
+    }
+}
